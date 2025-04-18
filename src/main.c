de1ad32106fcc4099dd54eeae4842d46ed210328
Felipe Caldas Marques Nasser <103406806+afsser@users.noreply.github.com>
2025-04-18 19:26:55 -0300

    :pencil: draft: added test map

--- src/main.c
+++ src/main.c
@@ -13,9 +13,30 @@
 // 	game->steps = 0;
 // }
 
+char **get_map(void)
+{
+	static char **map = {
+	"1111111111111",
+	"1100001000011",
+	"1000000000001",
+	"100000S000001",
+	"1000000000001",
+	"1100001000011",
+	"1000000000001",
+	"1000000000001",
+	"1000000000001",
+	"1100001000011",
+	"1111111111111",
+	NULL
+	};
+	return (map);
+}
+
 int32_t main(int argc, char **argv)
 {
-	// char **new_map;
+	char **new_map;
+
+	new_map = get_map();
 	// int i;
 	t_game game;
 
