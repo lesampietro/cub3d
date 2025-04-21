e003b59d50863f0c9978977b1fbd79c9278aa9cb
felipe nasser <felipecmnasser@gmail.com>
2025-04-21 00:22:29 -0300

    :pencil: merge: resolving conflicts

--- src/main.c
+++ src/main.c
@@ -13,8 +13,31 @@ void init_values(t_game *game)
 	// game->pos.y = 0;
 }
 
+char **get_map(void)
+{
+	static char **map = {
+	"1111111111111",
+	"1100001000011",
+	"1000000000001",
+	"100000S000001",
+	"1000000000001",
+	"1100001000011",
+	"1000000000001",
+	"1000000000001",
+	"1000000000001",
+	"1100001000011",
+	"1111111111111",
+	NULL
+	};
+	return (map);
+}
+
 int32_t main(int argc, char **argv)
 {
+	char **new_map;
+
+	new_map = get_map();
+	// int i;
 	t_game game;
 
 	init_values(&game);
