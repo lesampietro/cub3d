ef9888aebf3f2bc569100364d3e04dd0c3104072
Letícia <lesampietro@gmail.com>
2025-04-21 01:14:31 -0300

    :pencil: draft: safe_open and small refactoring

--- src/validation/map_validation.c
+++ src/validation/map_validation.c
@@ -1,62 +1,75 @@
 #include "../../includes/cub3d.h"
 
-void    is_valid_ext(char *file_ext)
+void	is_valid_ext(char *file_ext)
 {
 	char	*valid_ext;
-    int     i;
-    int     len;
+	int		i;
+	int		len;
 
-    i = 0;
+	i = 0;
 	valid_ext = ".cub";
-    len = ft_strlen(file_ext);
-    len = len - 4;
-    while (file_ext[len] && valid_ext[i] && file_ext[len] == valid_ext[i])
-    {
-        i++;
-        len++;
-    }
-    if (valid_ext[i] == '\0' && file_ext[len] == '\0')
-        return ;
-    printf(BPINK"Map extension is invalid"RST);
-    exit(EXIT_FAILURE);
+	len = ft_strlen(file_ext);
+	len = len - 4;
+	while (file_ext[len] && valid_ext[i] && file_ext[len] == valid_ext[i])
+	{
+		i++;
+		len++;
+	}
+	if (valid_ext[i] == '\0' && file_ext[len] == '\0')
+		return ;
+	printf(BPINK"Map extension is invalid"RST);
+	exit(EXIT_FAILURE);
 }
 
-void	check_path(char *path)
+void	check_args(int argc, char **argv)
 {
-	int	fd;
-
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-    {
-        printf(BPINK"Failure opening map"RST);
-		exit(EXIT_FAILURE);
-    }
-	close(fd);
-	return ;
-}
-
-void    check_args(int argc, char **argv)
-{
-    if (argc < 2)
+	if (argc < 2)
 	{
 		printf(BPINK"Too few arguments: please provide a path to a map"RST);
 		exit(EXIT_FAILURE);
 	}
-    else  if (argc > 2)
-    {
-        printf(BPINK"Too many arguments"RST);
-        exit(EXIT_FAILURE);
-    }
-    else
-        return ;
+	else if (argc > 2)
+	{
+		printf(BPINK"Too many arguments"RST);
+		exit(EXIT_FAILURE);
+	}
+	else
+		return ;
 }
 
-void    validate_map(int argc, char   **argv)
+int safe_open(char *filename)
 {
-    char    **map;
+	int fd;
 
-    map = NULL;
-    check_args(argc, argv);
-    check_path(argv[1]);
-    is_valid_ext(argv[1]); 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		printf(BPINK"Error: failed to open file"RST);
+		//funçao para lidar com erro - dar free e sair
+		exit(EXIT_FAILURE);
+	}
+	return (fd);
+}
+
+void	read_map(char *map_file, t_game *game)
+{
+	int		fd;
+	char	*tmp;
+
+	tmp = NULL;
+	fd = safe_open(map_file);
+	
+		
+
+}
+
+void	validate_map(int argc, char **argv, t_game *game)
+{
+	char	**map;
+
+	map = NULL;
+	check_args(argc, argv);
+
+	is_valid_ext(argv[1]);
+	read_map(argv[1], game);
 }
